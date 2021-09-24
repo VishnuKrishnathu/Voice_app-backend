dd350b624da7cedd91c17e36dc15ce3a18f30231
Vishnu Krishnathu <vishnu.krishnathu1999@gmail.com>
2021-09-24 20:12:47 +0530

    cors error fixed

--- app.ts
+++ app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['https://chatapplication-vishnukrishnathu.vercel.app', 'https://chatapplication-git-main-vishnukrishnathu.vercel.app', 'https://chatapplication-puce.vercel.app'],
     credentials: true
 }));
 
